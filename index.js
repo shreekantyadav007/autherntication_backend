6d5b22a523bbae495a8aa1ccf10dfc56451755c7
Shreekant <shreey94@gmail.com>
2024-11-29 10:41:35 +0530

    bcryptjs installed

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const cors = require("cors");
 
 const app = express();
